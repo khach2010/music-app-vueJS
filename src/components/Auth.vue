94ec2b9463d36438e18632a1d63193a278b511a5
tu le <khach2010@gmail.com>
2021-08-09 17:17:39 +0200

    Showing Alerts

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -195,6 +195,7 @@ export default {
 
       this.reg_alert_variant = 'bg-green-500';
       this.reg_alert_msg = 'Success! Your account has been created';
+      console.log(values);
     },
   },
 };
