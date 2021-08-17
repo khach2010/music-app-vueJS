000cbf542fae544ceafbdba703ee2eb0b3568d1b
tu le <khach2010@gmail.com>
2021-08-17 06:20:33 +0200

    handling Files - uploading Files to firebase - firebase rules and validation

--- src/includes/firebase.js
+++ src/includes/firebase.js
@@ -1,6 +1,7 @@
 import firebase from 'firebase'
 import 'firebase/auth'
 import 'firebase/firestore'
+import 'firebase/storage'
 
 const firebaseConfig = {}
 
@@ -10,6 +11,7 @@ firebase.initializeApp(firebaseConfig)
 // assign the auth and firestore reference from firebase to variables
 const auth = firebase.auth()
 const db = firebase.firestore()
+const storage = firebase.storage()
 const userCollection = db.collection('users')
 
-export { auth, db, userCollection }
+export { auth, db, userCollection, storage }
