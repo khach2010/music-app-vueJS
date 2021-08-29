f14c86d6c23a97de230b694fe8b5c93fd8915773
tu le <khach2010@gmail.com>
2021-08-29 08:31:10 +0200

    updating the comments list

--- src/views/Song.vue
+++ src/views/Song.vue
@@ -45,7 +45,7 @@
         </vee-form>
 
         <!-- Sort Comments -->
-        <select
+        <select v-model="sort"
           class="block mt-4 py-1.5 px-3 text-gray-800 border border-gray-300 transition
           duration-500 focus:outline-none focus:border-black rounded">
           <option value="1">Latest</option>
@@ -57,7 +57,7 @@
   <!-- Comments -->
   <ul class="container mx-auto">
     <li class="p-6 bg-gray-50 border border-gray-200"
-    v-for="comment in comments" :key="comment.docID">
+    v-for="comment in sortedComments" :key="comment.docID">
       <!-- Comment Author -->
       <div class="mb-5">
         <div class="font-bold">{{comment.name}}</div>
@@ -88,7 +88,19 @@ export default {
       comment_alert_variant: 'bg-blue-500',
       comment_alert_message: 'Please wait! Your comment is being submmitted',
       comments: [],
+      sort: '1',
     };
+  },
+  computed: {
+    ...mapState(['userLoggedIn']),
+    sortedComments() {
+      return this.comments.slice().sort((a, b) => {
+        if (this.sort === '1') {
+          return new Date(b.datePosted) - new Date(a.datePosted);
+        }
+        return new Date(a.datePosted) - new Date(b.datePosted);
+      });
+    },
   },
   async created() {
     const docSnapshot = await songsCollection.doc(this.$route.params.id).get();
@@ -100,9 +112,6 @@ export default {
 
     this.song = docSnapshot.data();
     this.getComments();
-  },
-  computed: {
-    ...mapState(['userLoggedIn']),
   },
   methods: {
     async addComment(values, { resetForm }) {
@@ -120,6 +129,8 @@ export default {
       };
 
       await commentsCollection.add(comment);
+
+      this.getComments();
 
       this.comment_in_submission = false;
       this.comment_alert_variant = 'bg-green-500';
