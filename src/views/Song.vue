6bbdf29fe7f1940c2a617a7e6bca7c058c8a30e0
tu le <khach2010@gmail.com>
2021-08-30 08:37:12 +0200

    Detecting Query Parameters

--- src/views/Song.vue
+++ src/views/Song.vue
@@ -110,6 +110,9 @@ export default {
       return;
     }
 
+    const { sort } = this.$route.query;
+    this.sort = sort === '1' || sort === '2' ? sort : '1';
+
     this.song = docSnapshot.data();
     this.getComments();
   },
@@ -148,6 +151,18 @@ export default {
           docID: doc.id,
           ...doc.data(),
         });
+      });
+    },
+  },
+  watch: {
+    sort(newVal) {
+      if (newVal === this.$route.query.sort) {
+        return;
+      }
+      this.$router.push({
+        query: {
+          sort: newVal,
+        },
       });
     },
   },
