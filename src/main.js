a8a2b33858023a0d8b561a8d2ed01b46e34272b8
tu le <khach2010@gmail.com>
2021-09-07 06:48:39 +0200

    Directives - Modifiers

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import VeeValidationPlugin from './includes/validation'
+import Icon from './directives/icon'
 import { auth } from './includes/firebase'
 import './assets/tailwind.css'
 import './assets/main.css'
@@ -15,6 +16,7 @@ auth.onAuthStateChanged(() => {
     app.use(store)
     app.use(router)
     app.use(VeeValidationPlugin)
+    app.directive('icon', Icon)
     app.mount('#app')
   }
 })
