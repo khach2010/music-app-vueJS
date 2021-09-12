214eecdb041a2909239d64da9c543b4699b81cb2
tu le <khach2010@gmail.com>
2021-09-12 07:56:48 +0200

    Auto Registering Global Components

--- src/main.js
+++ src/main.js
@@ -1,24 +1,26 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router';
-import store from './store';
-import VeeValidationPlugin from './includes/validation';
-import Icon from './directives/icon';
-import { auth } from './includes/firebase';
-import './assets/tailwind.css';
-import './assets/main.css';
-import i18n from './includes/i18n';
-import './registerServiceWorker';
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import VeeValidationPlugin from './includes/validation'
+import Icon from './directives/icon'
+import { auth } from './includes/firebase'
+import './assets/tailwind.css'
+import './assets/main.css'
+import i18n from './includes/i18n'
+import './registerServiceWorker'
+import GlobalComponents from './includes/_globals'
 
-let app;
+let app
 
 auth.onAuthStateChanged(() => {
   if (!app) {
-    app = createApp(App).use(i18n);
-    app.use(store);
-    app.use(router);
-    app.use(VeeValidationPlugin);
-    app.directive('icon', Icon);
-    app.mount('#app');
+    app = createApp(App).use(i18n)
+    app.use(store)
+    app.use(router)
+    app.use(VeeValidationPlugin)
+    app.use(GlobalComponents)
+    app.directive('icon', Icon)
+    app.mount('#app')
   }
-});
+})
