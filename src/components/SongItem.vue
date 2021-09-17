dfdca5030b7287392a22f8c27c21280619e07a4d
tu le <khach2010@gmail.com>
2021-09-17 09:33:59 +0200

    test children componenst - mocking methods - testing attributes

--- src/components/SongItem.vue
+++ src/components/SongItem.vue
@@ -1,6 +1,6 @@
 <template>
   <li class="flex justify-between items-center p-3 pl-6 cursor-pointer transition
-    duration-300 hover:bg-gray-50">
+    duration-300 hover:bg-gray-50 example" :class="`song-id-${song.docID}`">
     <div>
       <router-link
       :to="{name: 'song', params: {id: song.docID}}"
@@ -10,14 +10,14 @@
     </div>
 
      <div class="text-gray-600 text-lg">
-      <router-link custom
-        :to="{ name: 'song', params: { id: song.docID }, hash: '#comments' }"
-        v-slot="{ navigate }">
-        <span class="comments" @click="navigate">
-          <i class="fa fa-comments text-gray-600"></i>
-          {{ song.comment_count }}
-        </span>
-      </router-link>
+        <router-link custom
+          :to="{ name: 'song', params: { id: song.docID }, hash: '#comments' }"
+          v-slot="{ navigate }">
+          <span class="comments" @click="navigate">
+            <i class="fa fa-comments text-gray-600"></i>
+            {{ song.comment_count }}
+          </span>
+        </router-link>
     </div>
   </li>
 </template>
